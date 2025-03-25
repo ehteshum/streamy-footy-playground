ed79190df8f7f819fe4c379b642b5fc8350a1cc5
Ehteshum <37136242+ehteshum@users.noreply.github.com>
2025-03-26 05:20:29 +0600

    Update streams.ts

--- src/data/streams.ts
+++ src/data/streams.ts
@@ -11,10 +11,10 @@ export interface StreamData {
 export const predefinedStreams: StreamData[] = [
   {
     id: "premier-league-1",
-    url: "https://vsd115.okcdn.ru/hls/8060523776567.m3u8/sig/8tgx4l_1RnA/expires/1742996081440/srcIp/92.62.121.212/urls/45.136.22.91/clientType/1/srcAg/CHROME_ANDROID/mid/9244018679351/video.m3u8?p",
-    title: "BD VS IND",
+    url: "https://nix1-dc4-cdnnew.koskoros.ru/nix1-dc4-cdn/mono41/mono.m3u8",
+    title: "Argentina Vs Brazil",
     league: "Premier League",
-    description: "Bangladesh vs India"
+    description: "Argentina Vs Brazil"
   },
   {
     id: "Sony Aath",

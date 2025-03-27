0d145f6ad9045d5f0616da9f8dda79bd5a900196
Ehteshum <37136242+ehteshum@users.noreply.github.com>
2025-03-28 02:06:37 +0600

    Update streams.ts

--- src/data/streams.ts
+++ src/data/streams.ts
@@ -16,14 +16,14 @@ export const predefinedStreams: StreamData[] = [
     league: "Premier League",
     description: ""
   },
- /* {
-    id: "Sony Aath",
-    url: "https://live-test.tsports.com/live-01/master_480.m3u8?hdntl=Expires=1742996874~_GO=Generated~URLPrefix=aHR0cHM6Ly9saXZlLXRlc3QudHNwb3J0cy5jb20v~Signature=AXH73bb41230MDxlnngSsmBZoonAidwSTpjbrBmi8yiF_bTkbPaCW61aO60zirW7TacRgATW0Okpy7psP1ogO-LIOskF",
-    title: "BD VS IND - 2",
-    league: "La Liga",
-    description: "Bangladesh vs India"
-  },
   {
+    id: "Sony Aath",
+    url: "https://director.taihotel.asia/v3/director/VE1NDRkMGVjY2U4OWFlLTFiODgtMzc1NC04NWZhLTI1YzkwMWVl/master.m3u8?md5=zv0KAjO3bvXEpWPy0WkjZA&expires=1743148500&ddg=1",
+    title: "Barcelona Vs Osasuna",
+    league: "La Liga",
+    description: ""
+  },
+/*  {
     id: "bundesliga-1",
     url: "https://love2live.wideiptv.top/DSTVCRICKET/index.fmp4.m3u8?token=42e2e4ed8519acfe582433a9714ee8263082abb3-59b9ec75ffc76070294e8e751d0d391c-1743022318-1743011518",
     title: "IPL - Test",
